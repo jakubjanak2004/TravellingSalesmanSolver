aaa544e3f9f7201bd8faa7a4a26072b6a6f7dd3b
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-07 20:20:14 +0100

    arg handling logic removed from main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
 #include "controller/Controller.h"
 
-#include <iostream>
-#include <random>
-
 int main(int argc, char *argv[]) {
-    Controller::printHeader();
-    for (int i = 1; i < argc; i++) {
-        if (std::string(argv[i]) == "--help") {
-            Controller::showHelp();
-            return 0;
-        }
-    }
-
-    Controller controller = Controller();
+    auto controller = Controller();
     controller.run(argc, argv);
-
-    // testing the solver on a generated graph
-    // test_BB();
 }
