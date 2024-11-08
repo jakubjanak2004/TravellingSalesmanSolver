2014f6da7b07887c1d620c115c04dc4f77d23370
Jakub Janak <janakja5@fel.cvut.cz>
2024-11-08 10:29:48 +0100

    main updated to catch

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,1 @@
-//
-// Created by Jakub Janak on 9/19/24.
-//
-
-#include <gtest/gtest.h>
-
-int main (int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+#define CATCH_CONFIG_MAIN
