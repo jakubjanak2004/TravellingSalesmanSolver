cfafb2c81f67f4f7204b718c0dfc2ab1f1eef92f
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-11 21:49:00 +0100

    methods in file manager made private

--- files/file_manager.hpp
+++ files/file_manager.hpp
@@ -10,22 +10,22 @@
 #include "../graph/ts_instance.hpp"
 
 class file_manager {
+    static void load_graph_into_program(Agraph_t *graph, std::map<std::string, std::shared_ptr<node> > &nodesMap, std::vector<std::shared_ptr<edge> > &edges);
+
+    static bool load_graph(const std::string &file_name, GVC_t *gvc, Agraph_t *&graph, std::unique_ptr<ts_instance> &value1);
+
+    static void folder_exists_check();
+
+    static void file_naming_conflicts_check(const std::string &file_name, std::string &output_file);
+
 public:
     static const std::string INSTANCES_PATH;
 
     static const std::string RESULTS_PATH;
 
-    static bool load_graph(const std::string &file_name, GVC_t *gvc, Agraph_t *&graph, std::unique_ptr<ts_instance> &value1);
-
-    static void load_graph_into_program(Agraph_t *graph, std::map<std::string, std::shared_ptr<node> > &nodesMap, std::vector<std::shared_ptr<edge> > &edges);
-
     static std::unique_ptr<ts_instance> read_dot_file(const std::string &file_name);
 
     static std::vector<std::filesystem::directory_entry> get_dot_instances(const std::string &directory_path);
-
-    static void folder_exists_check();
-
-    static void file_naming_conflicts_check(const std::string &file_name, std::string &output_file);
 
     static void save_solution(const std::string &file_name, const std::string &file_content);
 };
